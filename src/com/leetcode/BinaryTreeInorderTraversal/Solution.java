cba8cab26e74025cb479373279f8a888c659d2dc
tangzhiyi11 <854742740@qq.com>
2014-12-07 20:36:09 +0800

    递归和非递归版

--- src/com/leetcode/BinaryTreeInorderTraversal/Solution.java
+++ src/com/leetcode/BinaryTreeInorderTraversal/Solution.java
@@ -2,6 +2,7 @@ package com.leetcode.BinaryTreeInorderTraversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import com.leetcode.BinaryTreeInorderTraversal.*;
 
@@ -11,13 +12,17 @@ public class Solution {
 		TreeNode root = new TreeNode(1);
 		TreeNode first = new TreeNode(2);
 		TreeNode second = new TreeNode(3);
+		TreeNode k = new TreeNode(4);
+		root.left = k;
 		root.right = first;
 		first.left = second;
-		List<Integer> result = inorderTraversal(root);
+		List<Integer> result = inorderTraversal2(root);
 		System.out.println(result);
 
 	}
-
+	/*
+	 * ÖÐÐò±éÀú µÝ¹é°æ
+	 */
 	public static List<Integer> inorderTraversal(TreeNode root){
 		List<Integer> result = new ArrayList<Integer>();
 		List<Integer> left = new ArrayList<Integer>();
@@ -33,9 +38,31 @@ public class Solution {
 		
 		
 	}
-	
+	/**
+	 * ÖÐÐò±éÀú·ÇµÝ¹é
+	 * @param root
+	 * @return List
+	 */
 	public static List<Integer> inorderTraversal2(TreeNode root){
 		List<Integer> result = new ArrayList<Integer>();
+		if(root==null)
+			return result;
+		TreeNode p = root;
+		TreeNode q;
+		Stack<TreeNode> s = new Stack<TreeNode>();
+
+		while(p!=null || !s.isEmpty())
+		{
+			while(p!=null ){
+				s.push(p);
+				p = p.left;
+
+			}
+			q = s.pop();
+			result.add(q.val);
+			p = q.right;
+
+		}
 		
 		return result;
 		
